4c61f6400318d17d0f3a0c011531f31c9604d22e
abinash <abinashphulkonwar1@gmail.com>
2023-12-17 22:02:10 +0530

    add keys schema

---
+++ src/db/keys.ts
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+interface Attrs {
+  public_key: string;
+  userId: mongoose.Schema.Types.ObjectId;
+}
+
+interface Doc extends mongoose.Document {
+  _id: mongoose.Schema.Types.ObjectId;
+  public_key: string;
+  userId: mongoose.Schema.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+interface Module extends mongoose.Model<Doc> {
+  build(attrs: Attrs): Doc;
+}
+
+const DocSchema = new Schema(
+  {
+    public_key: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// module.exports = mongoose.model("Chat", ChatSchema);
+// module.exports.chatSchema = ChatSchema;
+const Key = mongoose.model<Doc, Module>("Key", DocSchema);
+
+DocSchema.statics.build = (attrs: Attrs) => {
+  if (!attrs.public_key) throw new Error("public_key is required");
+  if (!attrs.userId) throw new Error("userId is required");
+
+  return new Key(attrs);
+};
+
+export { Key };
