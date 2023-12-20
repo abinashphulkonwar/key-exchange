2fc0286753a467dd7f12a8f5c28fd7bdc2ea18ad
abinash <abinashphulkonwar1@gmail.com>
2023-12-20 19:36:24 +0530

    add cooki session for auth

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 import { authRouter } from "./api/routes/auth";
+import cookieSession from "cookie-session";
 
 const app = express();
 const server = createServer(app);
@@ -11,6 +12,15 @@ io.on("connection", (socket) => {
   console.log("a user connected");
 });
 
+app.set("trust proxy", 1); // trust first proxy
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 1000 * 60 * 60 * 24 * 30 * 12 * 2,
+  })
+);
 app.use("/api/auth", authRouter);
 app.all("*", (req, res) => {
   res.status(404).send("not found");
