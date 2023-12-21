0d7e263962e93900889e99c14843e1caab442546
abinash <abinashphulkonwar1@gmail.com>
2023-12-21 20:34:09 +0530

    add current user route

--- api/src/api/routes/auth.ts
+++ api/src/api/routes/auth.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { signupController } from "../controllers/auth";
+import {
+  currentUserController,
+  loginController,
+  signupController,
+} from "../controllers/auth";
 
 const router = express.Router();
 
 router.post("/signup", signupController);
-router.post("/login", signupController);
+router.post("/login", loginController);
+router.get("/current-user", currentUserController);
 
 export { router as authRouter };
