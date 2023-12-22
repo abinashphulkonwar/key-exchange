cf7fea927c01fd045bc07af5cf6ad7017277e91c
abinash <abinashphulkonwar1@gmail.com>
2023-12-22 19:29:10 +0530

    add current user and required auth

---
+++ api/src/services/current-user.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from "express";
+import { V3 } from "paseto";
+import { User } from "../db/user";
+import { KeyPasto } from "./token";
+import { ApplicationError } from "./application-error";
+
+interface UserPayload {
+  _id: string;
+  iat: number;
+  exp: number;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      user: UserPayload;
+    }
+  }
+}
+
+export const currentUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.session?.id;
+  if (!token) return next();
+  const payload = (await V3.decrypt(token, KeyPasto)) as UserPayload;
+  req.user = payload;
+  next();
+};
+
+export const requiredAuth = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.user) throw new ApplicationError("Not authorized", 403);
+
+  next();
+};
