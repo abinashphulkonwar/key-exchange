a1bea495ee6c48438da3da4d8a5178f205916a78
abinash <abinashphulkonwar1@gmail.com>
2023-12-22 19:28:35 +0530

    add chat api

--- api/src/api/routes/auth.ts
+++ api/src/api/routes/auth.ts
@@ -4,11 +4,12 @@ import {
   loginController,
   signupController,
 } from "../controllers/auth";
+import { currentUser, requiredAuth } from "../../services/current-user";
 
 const router = express.Router();
 
 router.post("/signup", signupController);
 router.post("/login", loginController);
-router.get("/current-user", currentUserController);
+router.get("/current-user", currentUser, requiredAuth, currentUserController);
 
 export { router as authRouter };
