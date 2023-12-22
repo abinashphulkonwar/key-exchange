65334b40ece4ee15b877914b86345f113f5b646b
abinash <abinashphulkonwar1@gmail.com>
2023-12-22 19:29:34 +0530

    add auth check

--- application/src/pages/index.tsx
+++ application/src/pages/index.tsx
@@ -1,29 +1,51 @@
-import { Box, Heading } from "@chakra-ui/react";
-import { useLoaderData } from "react-router-dom";
+import { Box, Flex, Heading, Spacer, Text } from "@chakra-ui/react";
+import { useLoaderData, Link } from "react-router-dom";
+import { getCurrentUser } from "../web-api/user";
+import { getUsersList } from "../web-api/get-users-list";
 
 export const Loader = async ({}: { request: Request }) => {
-  const req = await fetch("/api/auth/current-user", {
-    method: "GET",
-    headers: {
-      "content-type": "application/json",
-    },
-  });
-  if (!req.ok) {
-    return {
-      isLogin: false,
-    };
-  }
-  const data = await req.json();
-  data.isLogin = true;
-  return data;
+  console.log("Loader");
+
+  const user = await getCurrentUser();
+  console.log(user);
+  const users_list = await getUsersList();
+
+  return {
+    user: user,
+    users_list: users_list,
+  };
 };
 
 export const Index = () => {
-  const data = useLoaderData();
-  console.log(data);
+  const { user, users_list } = useLoaderData() as {
+    user: { isLogin: boolean };
+    users_list: { _id: string; email: string }[];
+  };
+
+  if (!user.isLogin) {
+    return (
+      <Box>
+        <Heading>Not Loged In</Heading>
+        <Box h={20} />
+        <Flex>
+          <Link to={"/auth/login"}>Login</Link>
+          <Spacer />
+          <Link to={"/auth/signup"}>Create Account</Link>
+        </Flex>
+      </Box>
+    );
+  }
   return (
     <Box>
       <Heading>Key Exchanger</Heading>
+      {users_list.map((user) => {
+        return (
+          <Box>
+            <Text>{user._id}</Text>
+            <Text>{user.email}</Text>
+          </Box>
+        );
+      })}
     </Box>
   );
 };
