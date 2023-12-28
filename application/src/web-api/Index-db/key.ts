96e793e680833511be1ed4f82a0bb59259f9efb3
abinash <abinashphulkonwar1@gmail.com>
2023-12-28 20:41:33 +0530

    add key schema

--- application/src/web-api/Index-db/key.ts
+++ application/src/web-api/Index-db/key.ts
@@ -1,36 +1,35 @@
-import { IDBPDatabase, IDBPObjectStore } from "idb";
+import { IDBPDatabase } from "idb";
+import { Schema } from "./schema";
 const key = "key";
-export class KeyDB {
-  private static connection: IDBPDatabase<unknown> | null = null;
-  private static db: IDBPObjectStore<
-    unknown,
-    ArrayLike<string>,
-    "key",
-    "versionchange"
-  > | null = null;
-  static check_db_connection() {
-    if (!KeyDB.connection) {
-      throw new Error("database not initialized");
-    }
 
-    if (!KeyDB.db) {
-      throw new Error("KeyDB not initialized");
-    }
-  }
+type keydb = {
+  id: number;
+  public_key: string;
+  created_at: Date;
+};
+type keydbAttars = {
+  public_key: string;
+  created_at: Date;
+};
+
+export class KeyDB {
+  private static ref: Schema<keydb, keydbAttars> | null = null;
+
   static async init(connection: IDBPDatabase<unknown>) {
-    if (KeyDB.connection) {
+    if (KeyDB.ref) {
       throw new Error("KeyDB already initialized");
     }
-
-    KeyDB.connection = connection;
-    if (!connection.objectStoreNames.contains(key)) {
-      console.log("creating key store");
-
-      const keydb = connection.createObjectStore(key, {
-        keyPath: "id",
-        autoIncrement: true,
-      });
-      KeyDB.db = keydb;
+    KeyDB.ref = new Schema({
+      connection: connection,
+      name: key,
+      indexKey: "id",
+      isAutoincrement: true,
+    });
+  }
+  static save(data: keydbAttars) {
+    if (!KeyDB.ref) {
+      throw new Error("KeyDB not initialized");
     }
+    return KeyDB.ref.save(data);
   }
 }
