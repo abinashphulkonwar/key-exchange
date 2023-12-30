9f69e9338b52031d08e1d4074f165d2b44c93f9f
abinash <abinashphulkonwar1@gmail.com>
2023-12-30 18:55:02 +0530

    add index featire

--- application/src/web-api/Index-db/schema.ts
+++ application/src/web-api/Index-db/schema.ts
@@ -1,24 +1,58 @@
-import { IDBPDatabase } from "idb";
+import { IDBPDatabase, IDBPObjectStore } from "idb";
 interface db {
   connection: IDBPDatabase<unknown>;
   name: string;
   indexKey: string;
   isAutoincrement: boolean;
+  indexs?: {
+    field: string;
+    options?: IDBIndexParameters;
+    command: "create" | "delete" | "recreate";
+  }[];
 }
 export class Schema<TSchema, TAttrs> {
   protected connection: IDBPDatabase<unknown>;
   protected name: string;
-  constructor(db: db) {
+  constructor(db: db, isVersionChange: boolean) {
     this.verifyDB(db);
     this.connection = db.connection;
-    console.log("db init: ", db.name);
+
     this.name = db.name;
+    if (!isVersionChange) return;
+    let store_ref: IDBPObjectStore<
+      unknown,
+      ArrayLike<string>,
+      string,
+      "versionchange"
+    > | null = null;
     if (!this.connection.objectStoreNames.contains(db.name)) {
       console.log("creating key store");
 
-      this.connection.createObjectStore(db.name, {
+      store_ref = this.connection.createObjectStore(db.name, {
         keyPath: db.indexKey,
         autoIncrement: db.isAutoincrement,
+      });
+    }
+    if (!store_ref)
+      store_ref = this.connection.transaction(db.name, "versionchange").store;
+    if (db.indexs && db.indexs.length) {
+      db.indexs.forEach((index) => {
+        if (store_ref?.indexNames.contains(index.field)) {
+          if (index.command === "delete") {
+            store_ref?.deleteIndex(index.field);
+            return;
+          }
+          if (index.command === "recreate") {
+            store_ref.deleteIndex(index.field);
+          }
+        }
+        if (index.command === "delete") return;
+        if (store_ref) {
+          store_ref.createIndex(index.field, index.field, {
+            unique: index.options?.unique || false,
+            multiEntry: index.options?.multiEntry || false,
+          });
+        }
       });
     }
   }
