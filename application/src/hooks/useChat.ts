fb91d640eb872e160b4171bc49706a597079e07b
abinash <abinashphulkonwar1@gmail.com>
2023-12-30 20:32:15 +0530

    add init methode

--- application/src/hooks/useChat.ts
+++ application/src/hooks/useChat.ts
@@ -1,10 +1,36 @@
 import { chatSessionDB } from "../web-api/Index-db/chat-session";
+import { userDB } from "../web-api/Index-db/user";
 
-export const useChats = () => {
-  const init = () => {
+export const useChats = ({
+  _id,
+  name,
+}: {
+  id: number;
+  _id: string;
+  name: string;
+  profile: string;
+}) => {
+  const init = async () => {
     try {
-      const chatdb = chatSessionDB.find();
-      console.log(chatdb);
+      const user = userDB.findOne({
+        _id: _id,
+      });
+      if (!user) {
+        await userDB.save({
+          _id: _id,
+          name: name,
+          profile: "",
+        });
+      }
+      const chatdb = await chatSessionDB.findOne({
+        name: `chat:${_id}`,
+      });
+      if (!chatdb) {
+        await chatSessionDB.save({
+          name: `chat:${_id}`,
+          reciver_id: 0,
+        });
+      }
     } catch (error) {}
   };
 
