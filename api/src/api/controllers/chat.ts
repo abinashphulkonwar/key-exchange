06827d217afd389306ff67d0bcf0ca43e7c46b32
abinash <abinashphulkonwar1@gmail.com>
2023-12-30 20:31:57 +0530

    add chat filter

--- api/src/api/controllers/chat.ts
+++ api/src/api/controllers/chat.ts
@@ -4,15 +4,21 @@ import { User } from "../../db/user";
 import { V3 } from "paseto";
 import { KeyPasto } from "../../services/token";
 import { ApplicationError } from "../../services/application-error";
+import mongoose from "mongoose";
 
 export const userListController = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const users = await User.find({}).select({
+  const users = await User.find({
+    _id: {
+      $ne: req.user._id,
+    },
+  }).select({
     email: 1,
     _id: 1,
   });
+
   res.status(200).send(users);
 };
