192e156c8ad2a3b956d7c435b141ab272a4a5d11
abinash <abinashphulkonwar1@gmail.com>
2024-01-01 20:21:49 +0530

    add user data loader and setup user and save message

--- application/src/pages/chat/$_id.tsx
+++ application/src/pages/chat/$_id.tsx
@@ -7,18 +7,91 @@ import {
   Text,
   Textarea,
 } from "@chakra-ui/react";
-import { useLocation, useParams } from "react-router-dom";
+import { useEffect, useMemo, useState } from "react";
+import { useLoaderData, useLocation, useParams } from "react-router-dom";
+import { useChats } from "../../hooks/useChat";
+import { getCurrentUser } from "../../web-api/user";
+
+export const Loader = async ({ request }: { request: Request }) => {
+  try {
+    console.log("Loader: ", request.url);
+
+    const user = await getCurrentUser();
+    return user;
+  } catch (err) {
+    console.log(err);
+
+    return {
+      isLogin: null,
+    };
+  }
+};
 
 export const Index = () => {
+  const [message, setMessage] = useState<string>("");
   const params = useParams<{ _id: string }>();
-  const { state } = useLocation();
-  console.log(state);
+  const { state } = useLocation() as {
+    state: {
+      user: { _id: string; email: string };
+      device_user_id: string;
+    };
+  };
+
+  const loader = useLoaderData() as {
+    _id: string;
+    isLogin: boolean;
+  };
+
+  const user = useMemo(() => {
+    return {
+      _id: state.device_user_id || loader._id,
+    };
+  }, [loader, state]);
+
+  const useChat = useChats({
+    id: 0,
+    _id: params._id || "",
+    name: state?.user.email,
+    profile: "",
+  });
+
+  const init = async () => {
+    try {
+      await useChat.setupCurrentUser({ _id: user._id });
+
+      await useChat.init();
+    } catch (err: any) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    try {
+      init();
+    } catch (err: any) {
+      console.log(err.message);
+    }
+  }, []);
+
+  const onMessageHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+  };
+
+  const onSendHandler = async () => {
+    try {
+      console.log("send message: ", message);
+      await useChat.save(message);
+    } catch (err: any) {
+      console.log(err.message);
+    }
+  };
+
   return (
     <Box w={"100%"} h={"100%"} p={5}>
       <Flex flexDirection={"column"} h={"100%"}>
         <Box>
           <Heading as="h4" size="md" mb={2}>
-            {state?.email}
+            {state.user?.email}
           </Heading>
 
           <Text fontSize="sm">{params._id}</Text>
@@ -34,9 +107,11 @@ export const Index = () => {
               placeholder="Here is a sample placeholder"
               size="sm"
               resize={"none"}
+              value={message}
+              onChange={onMessageHandler}
             />
             <Box h={"100%"} w={5} />
-            <Button>Send</Button>
+            <Button onClick={onSendHandler}>Send</Button>
           </Flex>
         </Box>
       </Flex>
