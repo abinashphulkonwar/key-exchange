0c925406766aed93f8463c27ddb593157596b407
abinash <abinashphulkonwar1@gmail.com>
2024-01-02 21:04:59 +0530

    add key db schema

--- application/src/web-api/Index-db/key.ts
+++ application/src/web-api/Index-db/key.ts
@@ -1,16 +1,45 @@
 import { IDBPDatabase } from "idb";
-import { Schema } from "./schema";
+import { Schema, TCount } from "./schema";
 const key = "key";
+export const keydb_status_assigned = "assigned";
+export const keydb_status_unassigned = "unassigned";
 
+export const keydb_status_deleted = "deleted";
+export type keydb_status = "assigned" | "unassigned" | "deleted";
 type keydb = {
   id: number;
-  public_key: string;
+  public_key: JsonWebKey;
+  private_key: JsonWebKey;
   created_at: Date;
+  assigned_user__id: string;
+  assigned_user_id: IDBValidKey;
+  status: keydb_status;
+};
+type docQuery = {
+  id?: number;
+  public_key?: JsonWebKey;
+  private_key?: JsonWebKey;
+  assigned_user__id?: string;
+  assigned_user_id?: IDBValidKey;
+  status?: keydb_status;
 };
 type keydbAttars = {
-  public_key: string;
-  created_at: Date;
+  public_key: JsonWebKey;
+  private_key: JsonWebKey;
+  created_at?: Date;
+  status?: keydb_status;
 };
+
+interface TCountKey extends TCount {
+  field:
+    | "id"
+    | "assigned_user_id"
+    | "public_key"
+    | "assigned_user__id"
+    | "status"
+    | "created_at"
+    | "private_key";
+}
 
 export class KeyDB {
   private static ref: Schema<keydb, keydbAttars, any> | null = null;
@@ -30,6 +59,29 @@ export class KeyDB {
         name: key,
         indexKey: "id",
         isAutoincrement: true,
+        indexs: [
+          {
+            field: "assigned_user__id",
+            options: {
+              unique: false,
+            },
+            command: "recreate",
+          },
+          {
+            field: "assigned_user_id",
+            options: {
+              unique: false,
+            },
+            command: "recreate",
+          },
+          {
+            field: "public_key",
+            options: {
+              unique: false,
+            },
+            command: "recreate",
+          },
+        ],
       },
       isVersionChange
     );
@@ -38,12 +90,20 @@ export class KeyDB {
     if (!KeyDB.ref) {
       throw new Error("KeyDB not initialized");
     }
+    (data.created_at = new Date()), (data.status = "unassigned");
     return KeyDB.ref.save(data);
   }
-  static find() {
+  static find(query?: docQuery) {
     if (!KeyDB.ref) {
       throw new Error("KeyDB not initialized");
     }
-    return KeyDB.ref.find();
+    return KeyDB.ref.find(query);
+  }
+
+  static count(query: TCountKey) {
+    if (!KeyDB.ref) {
+      throw new Error("KeyDB not initialized");
+    }
+    return KeyDB.ref.count(query);
   }
 }
