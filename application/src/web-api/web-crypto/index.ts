6df6c53a2081a6a9ff0f56d7a7810dacf6822389
abinash <abinashphulkonwar1@gmail.com>
2024-01-03 12:38:07 +0530

    add push key feature

--- application/src/web-api/web-crypto/index.ts
+++ application/src/web-api/web-crypto/index.ts
@@ -1,13 +1,21 @@
-import { KeyDB, keydb_status_unassigned } from "../Index-db/key";
+import {
+  KeyDB,
+  keydb_status_pushed,
+  keydb_status_unassigned,
+} from "../Index-db/key";
 import { KeyPair } from "./key-pair";
 
 export class ApplicationCrypto {
   static async getNewKey() {
-    const count = await KeyDB.count({
+    const count_unassigned = await KeyDB.count({
       field: "status",
       value: keydb_status_unassigned,
     });
-    if (count <= 20) {
+    const count_pushed = await KeyDB.count({
+      field: "status",
+      value: keydb_status_pushed,
+    });
+    if (Math.max(count_unassigned, count_pushed) <= 20) {
       for (let i = 0; i < 50; i++) {
         const key_pair = await KeyPair.generateKeys();
         await KeyDB.save({
@@ -16,5 +24,47 @@ export class ApplicationCrypto {
         });
       }
     }
+  }
+  static async push_keys() {
+    const keys = await KeyDB.find({
+      status: keydb_status_unassigned,
+    });
+
+    const request = await fetch("/api/key/add", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        accept: "application/json",
+      },
+      body: JSON.stringify(
+        keys.map((val) => {
+          return {
+            device_key_id: val.id,
+            public_key: val.public_key,
+          };
+        })
+      ),
+    });
+
+    if (!request.ok) {
+      const res = await request.json();
+      console.log(res);
+      return;
+    }
+    const keys_updated_query = keys.map((val) => {
+      return {
+        id: val.id,
+      };
+    });
+    await KeyDB.findAndUpdate(
+      keys_updated_query,
+      keys.map((val) => {
+        return {
+          ...val,
+          status: keydb_status_pushed,
+        };
+      })
+    );
+    console.log("keys are pushed: ", keys.length);
   }
 }
