b11558c41c82aba9da195edd2bf2c3901272102c
abinash <abinashphulkonwar1@gmail.com>
2024-01-04 20:29:25 +0530

    add get key and ack on get key event

---
+++ api/src/handler/diffie-hellman .ts
@@ -0,0 +1,33 @@
+import { Socket } from "socket.io";
+import { Key } from "../db/keys";
+
+export const diffie_hellman = (socket: Socket) => {
+  socket.on("s-get-key-event", async (data: { userId: string }) => {
+    const key = await Key.findOne({ userId: data.userId, state: "unassigned" });
+    if (!key) return;
+    socket.emit("c-get-key-event", {
+      key: key?.public_key,
+      _id: key._id,
+      status: "ok",
+    });
+    await Key.findByIdAndUpdate(key._id, {
+      state: "send",
+      assigned_user_id: socket.user._id,
+    });
+  });
+  socket.on(
+    "s-ack-get-key-event",
+    async (data: { userId: string; _id: string }) => {
+      await Key.findOneAndUpdate(
+        {
+          _id: data._id,
+          assigned_user_id: socket.user._id,
+        },
+        { state: "assigned" }
+      );
+      socket.emit("c-ack-get-key-event", {
+        status: "ok",
+      });
+    }
+  );
+};
