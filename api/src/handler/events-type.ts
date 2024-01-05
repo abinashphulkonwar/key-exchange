01d6082724ddd66b9e2cb496f123ab57a8ce67d4
abinash <abinashphulkonwar1@gmail.com>
2024-01-05 17:55:22 +0530

    add events type

---
+++ api/src/handler/events-type.ts
@@ -0,0 +1,4 @@
+export const key_event = {
+  client: "c-key-event:get-other-user-key",
+  server: "s-key-event:sent-my-public-key",
+};
---
+++ application/src/context/events-type.ts
@@ -0,0 +1,5 @@
+export const key_event = {
+  client: "c-key-event:get-other-user-key",
+  server: "s-key-event:sent-my-public-key",
+  client_fetch: "s-get-key-event",
+};
