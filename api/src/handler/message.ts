6a7989342ed989b957bc14bb0cb4c9f17d44b352
abinash <abinashphulkonwar1@gmail.com>
2024-01-13 20:10:04 +0530

    add recipts event handler

--- api/src/handler/message.ts
+++ api/src/handler/message.ts
@@ -19,6 +19,52 @@ export const messages_handler = (socket: Socket) => {
         delivered_at: event.createdAt,
       });
     });
+
+    socket.on(
+      event_types.s_post_message_recipts,
+      async (data: {
+        to: string;
+        message_id: number;
+        command: "read" | "deliverd";
+        time: Date;
+        event_id: number;
+      }) => {
+        try {
+          const eventToUser = Events.build({
+            type: "get_recipts",
+            userId: socket.user._id,
+            state: "worker",
+            worker_process_count: 0,
+            recipts: {
+              from: socket.user._id,
+              to: data.to,
+              message_id: data.message_id,
+              command: "ack",
+              time: data.time,
+              event_id: data.event_id,
+            },
+          });
+          await eventToUser.save();
+          const eventToOther = Events.build({
+            type: "get_recipts",
+            userId: data.to,
+            state: "worker",
+            worker_process_count: 0,
+            recipts: {
+              from: socket.user._id,
+              to: data.to,
+              message_id: data.message_id,
+              command: data.command,
+              time: data.time,
+              event_id: data.event_id,
+            },
+          });
+          await eventToOther.save();
+        } catch (err: any) {
+          console.log(err.message);
+        }
+      }
+    );
   } catch (err: any) {
     console.log(err.message);
   }
